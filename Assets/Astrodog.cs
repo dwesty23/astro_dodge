45ba678ee3ede198e16f4218b7a986e8b2995574
Suhaan Syed <67168522+SuhaanSyed@users.noreply.github.com>
2024-01-30 10:50:19 -0600

    added dog, messed with asteroids, ufo can now follow dog

---
+++ Assets/Astrodog.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Astrodog : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal") * moveSpeed;
+        float y = Input.GetAxis("Vertical") * moveSpeed;
+
+        rb.AddForce(new Vector2(x, y));
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Asteroid")
+        {
+            Debug.Log("Game Over");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
