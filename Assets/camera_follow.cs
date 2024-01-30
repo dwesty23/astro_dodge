0bf2cb8f435cfd2afb2eeb83e2005f0178dc264b
dwesty23 <120140940+dwesty23@users.noreply.github.com>
2024-01-30 17:56:45 -0600

    got the camera code but no idea how to test...

---
+++ Assets/camera_follow.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class camera_follow : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+    public float smoothSpeed = 0.125f;
+
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = player.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+
+        transform.LookAt(player);
+    }
+}
