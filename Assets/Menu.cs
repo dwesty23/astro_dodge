e80af4f2d9dc0509215a76006c396a0eb4c5574d
Jacob Horton <99301327+hortojac@users.noreply.github.com>
2024-01-31 17:25:58 -0600

    basic menu placeholder

---
+++ Assets/Menu.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Menu : MonoBehaviour
+{
+    public void OnPlayButton ()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
