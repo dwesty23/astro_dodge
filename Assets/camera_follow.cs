423fe5d683cfd0c0cacda2e4b88c8acf449814de
dwesty23 <120140940+dwesty23@users.noreply.github.com>
2024-01-30 18:51:29 -0600

    works

--- Assets/camera_follow.cs
+++ Assets/camera_follow.cs
@@ -6,12 +6,10 @@ public class camera_follow : MonoBehaviour
     public Vector3 offset;
     public float smoothSpeed = 0.125f;
 
-    void FixedUpdate()
+    void LateUpdate()
     {
         Vector3 desiredPosition = player.position + offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
         transform.position = smoothedPosition;
-
-        transform.LookAt(player);
     }
 }
