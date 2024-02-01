658f2973010758bac57f21fbb08b9cd423a99cff
bhavik-goplani <56516858+bhavik-goplani@users.noreply.github.com>
2024-02-01 17:01:57 -0600

    Added part collection

--- Assets/Astrodog.cs
+++ Assets/Astrodog.cs
@@ -5,6 +5,9 @@ public class Astrodog : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
+    public bool hasPart = false;
+    public int partsColelcted = 0;
+
     void Update()
     {
         float x = Input.GetAxis("Horizontal") * moveSpeed;
@@ -20,5 +23,45 @@ public class Astrodog : MonoBehaviour
             Debug.Log("Game Over");
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
+
+        if (col.tag == "part1" || col.tag == "part2" || col.tag == "part3" || col.tag == "part4" || col.tag == "part5")
+        {
+            if(hasPart == false)
+            {
+                hasPart = true;
+                partsColelcted++;
+                Destroy(col.gameObject);
+                Debug.Log("You have collected a part");
+
+            }
+            else
+            {
+                Debug.Log("You already have a part");
+            }
+            
+        }
+
+        if (col.tag == "ship") {
+            if(hasPart == true)
+            {
+                hasPart = false;
+                col.GetComponent<ShipRepair>().RepairShip();
+                if (partsColelcted == 5)
+                {
+                    Debug.Log("You Win!");
+                }
+            }
+            else
+            {
+                Debug.Log("You need to collect all the parts");
+            }
+        }
+
+    }
+
+    bool PlayerHasPart()
+    {
+        return hasPart;
+
     }
 }
